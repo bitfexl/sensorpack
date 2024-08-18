27e706147a2082f40c28276c8e194345974c646e
bitfexl <54662051+bitfexl@users.noreply.github.com>
2024-08-18 11:10:39 +0200

    install capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+    appId: "com.github.bitfexl.sensorpack",
+    appName: "sensorpack",
+    webDir: "dist",
+};
+
+export default config;
